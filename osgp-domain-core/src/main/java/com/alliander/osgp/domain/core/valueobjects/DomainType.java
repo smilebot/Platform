b428cb21421cecd01778481907d72d5a9c732838
Sander van der Heijden <sander.van.der.heijden@cgi.com>
2016-04-21 16:08:34 +0200

    FLEX-1764 ~ Adds controllable load domain type

--- osgp-domain-core/src/main/java/com/alliander/osgp/domain/core/valueobjects/DomainType.java
+++ osgp-domain-core/src/main/java/com/alliander/osgp/domain/core/valueobjects/DomainType.java
@@ -8,5 +8,7 @@
 package com.alliander.osgp.domain.core.valueobjects;
 
 public enum DomainType {
-    PUBLIC_LIGHTING, TARIFF_SWITCHING
+    PUBLIC_LIGHTING,
+    TARIFF_SWITCHING, 
+    CONTROLLABLE_LOAD
 }
