66439a61c9e428e0cc1d5a391ab945279ef5ccce
Sander van der Heijden <sander.van.der.heijden@cgi.com>
2016-10-25 14:30:51 +0200

    ZS-108 ~ Adds headers, renames db files

--- osgp-domain-microgrids/src/main/java/com/alliander/osgp/domain/microgrids/entities/Task.java
+++ osgp-domain-microgrids/src/main/java/com/alliander/osgp/domain/microgrids/entities/Task.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright 2016 Smart Society Services B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
 package com.alliander.osgp.domain.microgrids.entities;
 
 import java.util.Date;
@@ -13,9 +20,6 @@ import com.alliander.osgp.shared.domain.entities.AbstractEntity;
 @Entity
 public class Task extends AbstractEntity {
 
-    /**
-     *
-     */
     private static final long serialVersionUID = -1231883978623839983L;
 
     @Column
