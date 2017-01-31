5bf39f107893c2eed92ce27b0bda3755d37b2dae
Kevin Smeets <kevin.smeets@cgi.com>
2017-01-31 11:23:43 +0000

    DA-14 ~ Adds docker containers for OSGP components.

---
+++ osgp-adapter-ws-core/dockerfy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+COMPONENT=osgp-adapter-ws-core
+docker build -t $COMPONENT ./
+docker run -it --rm -e "DBHOST=10.0.2.15" -e "DBPORT=5432" -e "JMSHOST=10.0.2.15" -e "JMSPORT=61616" -p 8009 $COMPONENT
